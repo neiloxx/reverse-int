291bdf8e6ab8fa8b8b66a9c501ffd3b13636911d
neiloxx <anastasia.v.sido@gmail.com>
2021-01-26 14:48:50 +0600

    feat: reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let newNum = '';
+    n = Math.abs(n).toString();
+    for (let i = n.length - 1; i >= 0; i--){
+        newNum += n[i];
+    }
+    return newNum;
 }
